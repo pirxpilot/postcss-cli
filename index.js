ca7c79c0a0a97722a29744604c8dac167099913f
Damian Krzeminski <pirxpilot@furkot.com>
2020-06-04 13:22:50 +0200

    export runner and argument parser
    if somone wants to reuse the runner or - even less likely - the argument
    parser code it is now possible:
    
        const { run } = require('postcss-cli-simple');
    
        const config = prepareMyConfig();
    
        run(config, exitCode => process.exit(exitCode));
    
    ref: #58

--- index.js
+++ index.js
@@ -1,10 +1,4 @@
-const argv = require('./lib/argv');
-const run = require('./lib/run');
-
-const args = argv();
-
-if (typeof args === 'number') {
-  process.exit(args);
-} else {
-  run(args, exitCode => process.exit(exitCode));
-}
+module.exports = {
+  argv: require('./lib/argv'),
+  run: require('./lib/run')
+};
